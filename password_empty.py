249e6353f124ddef391d961095b02912d0c0d813
Nicolò Poggi <85927974+pogginicolo98@users.noreply.github.com>
2021-08-01 13:22:03 +0200

    Add files via upload

---
+++ password_empty.py
@@ -0,0 +1,11 @@
+# settings.py
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'your_django_secret_key'
+# Email settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'your_email_host'
+EMAIL_PORT = 0
+EMAIL_HOST_USER = 'your_email_host_user'
+EMAIL_HOST_PASSWORD = 'your_host_password'
+DEFAULT_FROM_EMAIL = 'your_email_host_user'
+DEFAULT_TO_EMAIL = 'your_email_host_user'
